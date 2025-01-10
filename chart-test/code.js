46efa3ea61ead4a7cd3f515c5f9672b2d6cf6541
Halvard Knustad <halvard.knustad@asvg.no>
2025-01-10 14:10:30 +0100

    test av Chart.js
    Tester ut ulike diagrammer

--- chart-test/code.js
+++ chart-test/code.js
@@ -46,3 +46,56 @@ new Chart(ctx2, {
       }
     }
   });
+
+
+const ctx3 = document.getElementById('myChart3');
+
+const data = {
+  labels: [
+    'Eating',
+    'Drinking',
+    'Sleeping',
+    'Designing',
+    'Coding',
+    'Cycling',
+    'Running'
+  ],
+  datasets: [{
+    label: 'My First Dataset',
+    data: [65, 59, 90, 81, 56, 55, 40],
+    fill: true,
+    backgroundColor: 'rgba(255, 99, 132, 0.2)',
+    borderColor: 'rgb(255, 99, 132)',
+    pointBackgroundColor: 'rgb(255, 99, 132)',
+    pointBorderColor: '#fff',
+    pointHoverBackgroundColor: '#fff',
+    pointHoverBorderColor: 'rgb(255, 99, 132)'
+  }, {
+    label: 'My Second Dataset',
+    data: [28, 48, 40, 19, 96, 27, 100],
+    fill: true,
+    backgroundColor: 'rgba(54, 162, 235, 0.2)',
+    borderColor: 'rgb(54, 162, 235)',
+    pointBackgroundColor: 'rgb(54, 162, 235)',
+    pointBorderColor: '#fff',
+    pointHoverBackgroundColor: '#fff',
+    pointHoverBorderColor: 'rgb(54, 162, 235)'
+  }]
+};
+
+new Chart(ctx3, {
+  type: 'radar',
+  data: {
+    labels: ['Attack', 'Defense', 'Speed', 'Magic'],
+    datasets: [12, 1, 3, 4],
+  },
+  options: {
+    responsive: true,
+    plugins: {
+      title: {
+        display: true,
+        text: 'Chart.js Radar Chart'
+      }
+    }
+  },
+});

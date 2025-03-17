7325c4fcfba1feb90d706726316b6982eeebaa38
Halvard Knustad <halvard.knustad@asvg.no>
2025-03-17 14:31:47 +0100

    liten update

---
+++ api-lek/code.js
@@ -0,0 +1,12 @@
+let overskrift = document.createElement("h1");
+overskrift.innerText = "Dette er en overskrift";
+document.body.appendChild(overskrift);
+
+let paragraf = document.createElement("p");
+paragraf.innerText = "Dette er en paragraf";
+document.body.appendChild(paragraf);
+
+let bilde = document.createElement("img");
+bilde.src = "dancing.gif";
+document.body.appendChild(bilde);
+
